7bf93c1a7a15a3cc5f31cef616a31498b0e69f8f
Sean Grimes <spg63@drexel.edu>
2017-01-19 20:24:31 -0500

    removing ignored files

---
+++ cppcommon_proj_updater/cppcommon_proj_updater/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  cppcommon_proj_updater
+//
+//  Created by Sean Grimes on 1/19/17.
+//  Copyright © 2017 Sean Grimes. All rights reserved.
+//
+
+#include <iostream>
+#include "CmdLineParser.hpp"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
