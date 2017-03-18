ebe1983df1028f1c74f02cca25aaed4a1bad7528
Sean Grimes <spg63@drexel.edu>
2017-03-18 13:41:05 -0400

    updated docs

--- docs/search/all_e.js
+++ docs/search/all_e.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['randomdouble',['randomDouble',['../namespace_num_utils.html#a402b26db626888d32e79bfab0e6ba5c2',1,'NumUtils']]],
   ['randomint',['randomInt',['../namespace_num_utils.html#abb2bad9628db7cd63498680a6f84e13c',1,'NumUtils']]],
-  ['read_5fkernels_5ffrom_5ffile',['read_kernels_from_file',['../class_c_l_helper.html#a14ace996406fabec7232dc354b0aee27',1,'CLHelper::read_kernels_from_file(const char *fname, char **programBuffer)'],['../class_c_l_helper.html#a14ace996406fabec7232dc354b0aee27',1,'CLHelper::read_kernels_from_file(const char *fname, char **programBuffer)']]],
+  ['read_5fkernels_5ffrom_5ffile',['read_kernels_from_file',['../class_c_l_helper.html#a14ace996406fabec7232dc354b0aee27',1,'CLHelper']]],
   ['readfullfile',['readFullFile',['../namespace_file_utils.html#a2396a07d5e0f89d90346bd2d15cca1e5',1,'FileUtils']]],
   ['readlinebyline',['readLineByLine',['../namespace_file_utils.html#adebcd3db328c1717dfda0e58b414f5c2',1,'FileUtils']]],
   ['removecharfromstr',['removeCharFromStr',['../namespace_str_utils.html#a1d9112fe1be09aeafa3539fb771dd7c8',1,'StrUtils']]]
--- docs/search/functions_6.js
+++ docs/search/functions_6.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['get_5fopencl_5ferror',['get_opencl_error',['../class_c_l_helper.html#a91cb1425e4a3fc2edaa9cc4431b30f2f',1,'CLHelper::get_opencl_error(int err)'],['../class_c_l_helper.html#a91cb1425e4a3fc2edaa9cc4431b30f2f',1,'CLHelper::get_opencl_error(int err)']]],
+  ['get_5fopencl_5ferror',['get_opencl_error',['../class_c_l_helper.html#a91cb1425e4a3fc2edaa9cc4431b30f2f',1,'CLHelper']]],
   ['getallpts',['getAllPts',['../class_vec3.html#a026bab1473cf4b2bed779c31e1fd88df',1,'Vec3']]],
   ['getdirsindir',['getDirsInDir',['../namespace_file_utils.html#a9d05c9c25547907005ffea5848726e13',1,'FileUtils']]],
   ['getfilesindir',['getFilesInDir',['../namespace_file_utils.html#a184e1a050414a57d019f4e2b81fe4453',1,'FileUtils']]],
